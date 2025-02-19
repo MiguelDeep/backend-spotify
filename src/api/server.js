785ce58dc5da9e7e86c1f8141478f430804a1e55
Deep <110001574+MiguelDeep@users.noreply.github.com>
2025-02-19 19:16:23 +0100

    adicionando o .env

--- src/api/server.js
+++ src/api/server.js
@@ -1,8 +1,16 @@
 import express, { json } from "express";
 import { db } from "../database/database.js";
 import cors from "cors"
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
+
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.get("/", (request, response) => {
@@ -18,6 +26,6 @@ app.get("/songs", async (request, response) => {
   return response.json(await db.collection("songs").find({}).toArray());
 });
 
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log("Server is running on port 3001");
 });
